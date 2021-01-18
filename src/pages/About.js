26b5b8aea9622432470027293b6353b1aa254d56
djanuskaite <januskaite.domante@gmail.com>
2021-01-18 02:37:25 +0200

    some init msg

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+      A cocktail is an alcoholic mixed drink, which is either a combination of spirits, or one or more spirits mixed with other ingredients such as fruit juice, flavored syrup, or cream. There are various types of cocktails, based on the number and kind of ingredients added. The origins of the word cocktail have been debated. The first known written mention of cocktail as an alcoholic beverage appeared in The Balance and Columbian Repository (Hudson, New York) May 13, 1806.
+      </p>
+    </section>
+  )
+}
+
+export default About
